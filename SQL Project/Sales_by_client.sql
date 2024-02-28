a11558e312aa20a11610acfd555555c7a1185150
Pornpailin Krudsupat <141467571+BambiPK@users.noreply.github.com>
2024-02-28 23:35:13 +0700

    Add files via upload

---
+++ SQL Project/Sales_by_client.sql
@@ -0,0 +1,15 @@
+/* Revenue by customer */
+SELECT
+	customerid,
+    companyname,
+    SUM(SalesPrice) as Rev    
+FROM (
+SELECT
+	Customers.customerid,
+    companyname,
+	ROUND((unitprice * quantity) - (unitprice * quantity * discount),2) as SalesPrice
+    FROM Customers
+	JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+	JOIN Orders_Details on Orders.OrderID = Orders_Details.OrderID
+  	  )
+GROUP by 1,2;
